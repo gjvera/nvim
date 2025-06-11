2d33661c8516a6b0955597b8e27ee49e57b8a93b
Gabriel Vera <36056781+gjvera@users.noreply.github.com>
2025-06-11 11:48:36 -0700

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = ','
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
